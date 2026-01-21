aa720a61b6d53c2ad445757eb0418f0cf56ec266
Tareq1131 <tareqislam076@gmail.com>
2026-01-21 12:41:18 +0600

    backend update

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,21 @@
 import cookieParser from "cookie-parser";
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+import userRoute from "./routes/user.route.js";
+import companyRoute from "./routes/company.route.js";
+import jobRoute from "./routes/job.route.js";
+import applicationRoute from "./routes/application.route.js";
 
+dotenv.config({});
 const app = express();
-app.get("/home",(req,res)=>{
-    return res.status(200).json({
-        message:"I am comming from backend",
-        success: true
-    })
-});
+// app.get("/home",(req,res)=>{
+//     return res.status(200).json({
+//         message:"I am comming from backend",
+//         success: true
+//     })
+// });
 
 // middleware 
 app.use(express.json());
@@ -18,12 +25,20 @@ const corsOptions = {
     origin:'http://localhost:5173',
     credentials:true
 }
-
 app.use(cors(corsOptions));
 
 
-const PORT = 3004;
+const PORT = process.env.PORT || 3006;
+
+
+// api's
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/company", companyRoute);
+app.use("/api/v1/job", jobRoute);
+app.use("/api/v1/application", applicationRoute);
+
 app.listen(PORT,()=>{
+    connectDB();
  console.log(`Server running at port ${PORT}`);
 })
 
